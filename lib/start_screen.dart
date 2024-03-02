d1928129a116876c5085db1fd6ab73e0bc19d151
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2024-03-02 17:59:23 -0500

    Start screen completed

---
+++ lib/start_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return (Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+            height: 300,
+            color: const Color.fromRGBO(255, 255, 255, 0.5),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            "Welcome to the Impossible Quiz",
+            style: TextStyle(
+                color: Colors.white, fontSize: 24, fontWeight: FontWeight.w300),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 16),
+              foregroundColor: Colors.white,
+              fixedSize: const Size(160, 60),
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text("Start Quiz"),
+          )
+        ],
+      ),
+    ));
+  }
+}
